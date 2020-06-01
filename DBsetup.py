4b1fdd245bea403f7c434d3d0f73f3a6a65a0dcc
bensonmat8 <37485710+bensonmat8@users.noreply.github.com>
2020-06-01 17:17:21 -0400

    Changed the database structure

--- DBsetup.py
+++ DBsetup.py
@@ -3,9 +3,11 @@ import sqlite3
 conn = sqlite3.connect('employee.db')
 c = conn.cursor()
 c.execute("""create table employee (
-    e_num text,
-    first text,
-    last text,
+    uniq_id text primary key,
+    dept_id text,
+    emp_id text,
+    first_name text,
+    last_name text,
     title text,
     shift text,
     days_off text,
@@ -17,4 +19,20 @@ c.execute("""create table employee (
     )
     """)
 conn.commit()
+
+c.execute("insert into employee values ('10', '1', '0', 'Beeth', 'Bef', 'cook', 'AM', 'Sunday', 40.0, '5/23/20 11:14 PM', '5/23/20 11:14 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('11', '1', '1', 'Beeth', 'Acs', 'Ba', 'PM', 'Tuesday', 40.0, '5/23/20 11:14 PM', '5/23/20 11:14 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('12', '1', '2', 'Zen', 'Ae4b', 'Cook', 'AM/PM', 'Monday', 32.0, '5/23/20 11:18 PM', '5/23/20 11:18 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('13', '1', '3', 'Zen', 'Ben', 'cook', 'AM', 'Sunday', 24.0, '5/22/20 7:50 PM', '5/22/20 7:50 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('14', '1', '4', 'Beeth', 'Blam', 'Bill', 'AM', 'Tuesday', 20.0, '5/22/20 7:50 PM', '5/22/20 7:50 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('15', '1', '5', 'Zlaten', 'Zener', 'Bus', 'PM', 'Thursday', 40.0, '5/22/20 7:51 PM', '5/22/20 7:51 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('16', '1', '6', 'Chopin', 'SDF', 'Cr', 'AM/PM', 'Tuesday', 40.0, '5/22/20 7:57 PM', '5/22/20 7:57 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('17', '1', '7', 'Gomma', 'Bleo', 'Tend', 'AM', 'Thursday', 32.0, '5/22/20 7:58 PM', '5/22/20 7:58 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('18', '1', '8', 'Gomma', 'Bleo', 'Tend', 'AM', 'Thursday', 24.0, '5/22/20 12:00 AM', '5/22/20 12:00 AM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('19', '1', '9', 'Gomma', 'Gomez', 'Carlos', 'AM', 'Thursday', 20.0, '5/21/20 12:00 AM', '5/21/20 12:00 AM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('110', '1', '10', 'Zen', 'Ae4b', 'Cook', 'AM/PM', 'Monday', 40.0, '5/26/20 10:26 PM', '5/26/20 10:26 PM', 'Admin', 'Admin');")
+c.execute("insert into employee values ('111', '1', '11', 'Chopin', 'Ben', 'Cook', 'AM', 'Monday', 40.0, '5/26/20 10:32 PM', '5/26/20 10:32 PM', 'Admin', 'Admin');")
+
+conn.commit()
+
 conn.close()
