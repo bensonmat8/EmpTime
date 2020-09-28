441a456eb97cdacfcb916b9d663fa75a0689222a
bensonmat8 <37485710+bensonmat8@users.noreply.github.com>
2020-09-28 17:30:19 -0400

    Created separate js file to reuse submit_data funtion

---
+++ static/SubmitData.js
@@ -0,0 +1,26 @@
+function submit_data(_submit_link) {
+
+    var all = document.getElementsByClassName('sub_item')
+    for (var i = 0; i < all.length; i++) {
+        elm = all[i]
+        if (elm.value != "") {
+            var data = Object.assign({}, elm.dataset)
+            data['value'] = elm.value
+
+            fetch(_submit_link, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(data)
+            }).then(function (response) {
+                return response.text();
+            }).then(function (text) {
+                console.log('POST response: ');
+                console.log(text);
+                //alert(text);
+            })
+        }
+    }
+
+}
